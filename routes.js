ea0ddf2b67026a2f556d11b5d9f1a504947ff119
DevShubX <60731101+DevShubX@users.noreply.github.com>
2022-10-24 17:15:15 +0530

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const route = express.Router();
+
+const getLinks = require('./routes/getLinks');
+const mediaInfo = require('./routes/mediaInfo');
+const movie = require('./routes/movie');
+const search = require('./routes/search');
+const tv = require('./routes/tv');
+
+route.use('/search',search);
+route.use('/movie',movie);
+route.use('/tv',tv);
+route.use('/info',mediaInfo);
+route.use('/links',getLinks);
+
+module.exports = route;
---
+++ server.js
@@ -0,0 +1,26 @@
+const app = require('express')()
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
+
+const cors = require("cors")
+var corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+    port: PORT,
+}
+
+app.use(cors(corsOptions));
+
+// Root
+app.get('/', cors(),(req, res) => {
+    res.sendFile(`${__dirname}/index.html`)
+})
+
+const routes = require('./routes');
+
+app.use("/api",routes);
+
+app.listen(PORT, () => {
+	console.log(`It is listening on http://localhost:${PORT}`)
+})
